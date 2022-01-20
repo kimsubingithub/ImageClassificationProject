3d319be1a8d453ab12b89f78b4efae94100384cd
Subin Kim <kimsb8027@gmail.com>
2022-01-21 01:01:15 +0900

    commit5

--- lib/pokemonFind.dart
+++ lib/pokemonFind.dart
@@ -1,4 +1,6 @@
 import 'dart:html';
+import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -11,23 +13,59 @@ class PokemonFind extends StatefulWidget {
 }
 
 class _PokemonFindState extends State<PokemonFind> {
+  XFile? _image;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('포켓몬 찾기'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
       ),
       body: Container(
         child: Column(
           children: [
+            //
             Container(
               width: 300,
               height: 300,
-              child: ElevatedButton(
-                onPressed: () {},
+              child: InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: true, // 밖 선택시 닫기
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('이미지'),
+                        content: SingleChildScrollView(
+                          child: Text('이미지를 가져올 곳을 선택하시오.'),
+                        ),
+                        actions: <Widget> [
+                          ElevatedButton(
+                            child: Text('카메라'),
+                            onPressed: () {
+                              getImage(ImageSource.camera);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          ElevatedButton(
+                            child: Text('갤러리'),
+                            onPressed: () {
+                              getImage(ImageSource.gallery);
+                              Navigator.of(context).pop();
+                              //Image.asset(_image!.path, fit: BoxFit.contain);
+                            },
+                          ),
+                        ],
+                      );
+                    });
+                },
                 child: Text('찾고 싶은 포켓몬 이미지 가져오기'),
               ),
             ),
+            //
             Center(
               child: ElevatedButton(
                 onPressed: () {
@@ -54,6 +92,14 @@ class _PokemonFindState extends State<PokemonFind> {
         child: Icon(Icons.book),
       ),
     );
+  }
+
+  Future getImage(ImageSource imageSource) async {
+    final XFile? image = await _picker.pickImage(source: imageSource);
+
+    setState(() {
+      _image = XFile(image!.path);
+    });
   }
 
   void _DNavigation(BuildContext context) async {
